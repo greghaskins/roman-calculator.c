9847970b40bfa46cb919b26cad7854552f367ef4
Gregory Haskins <greg@greghaskins.com>
2016-08-12 23:03:40 +0000

    Add by repeating a numeral 2 or 3 times

--- src/roman_calculator.c
+++ src/roman_calculator.c
@@ -2,5 +2,7 @@
 
 char *roman_calculator_add(const char *left, const char *right)
 {
-	return "II";
+	char result[strlen(left) + strlen(right)];
+	sprintf(result, "%s%s", left, right);
+	return result;
 }
--- tests/addition_tests.c
+++ tests/addition_tests.c
@@ -2,15 +2,19 @@
 
 #include "../src/roman_calculator.h"
 
-START_TEST(one_and_one_is_two)
+START_TEST(can_add_by_simple_repetition)
 {
 	ck_assert_str_eq("II", roman_calculator_add("I", "I"));
+	ck_assert_str_eq("III", roman_calculator_add("I", "II"));
+	ck_assert_str_eq("XXX", roman_calculator_add("XX", "X"));
+	ck_assert_str_eq("CC", roman_calculator_add("C", "C"));
+	ck_assert_str_eq("MMM", roman_calculator_add("M", "MM"));
 }
 END_TEST
 
 TCase *addition_tests()
 {
 	TCase *test_case = tcase_create("addition tests");
-	tcase_add_test(test_case, one_and_one_is_two);
+	tcase_add_test(test_case, can_add_by_simple_repetition);
 	return test_case;
 }
