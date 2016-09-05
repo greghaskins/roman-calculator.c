918bbfb52377074b515d9208211c4515c6acbd9e
Gregory Haskins <greg@greghaskins.com>
2016-09-05 15:10:39 +0000

    Move roman cluster implementation to .c file
    This hides the implementation details from callers.

--- src/roman_clusters.c
+++ src/roman_clusters.c
@@ -1,5 +1,21 @@
 #include "roman_clusters.h"
 
+static const RomanCluster ROMAN_CLUSTERS[] = {
+	{ 1000, "M", 1 },
+	{ 900, "CM", 2 },
+	{ 500, "D", 1 },
+	{ 400, "CD", 2 },
+	{ 100, "C", 1 },
+	{ 90, "XC", 2 },
+	{ 50, "L", 1 },
+	{ 40, "XL", 2 },
+	{ 10, "X", 1 },
+	{ 9, "IX", 2},
+	{ 5, "V", 1 },
+	{ 4, "IV", 2 },
+	{ 1, "I", 1 }
+};
+
 static const RomanCluster *LARGEST_CLUSTER = &ROMAN_CLUSTERS[0];
 static const RomanCluster *SMALLEST_CLUSTER = &ROMAN_CLUSTERS[sizeof(ROMAN_CLUSTERS) / sizeof(RomanCluster) - 1];
 
